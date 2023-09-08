1e593a680cd00ba6ce842bf4b50556d523023171
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-09-08 12:58:55 +0530

    initial commit

---
+++ react-next/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import React, { useState } from "react";
+
+const page = () => {
+
+  return (
+    <>
+    <h1>This is Home Page</h1>
+     <a href="/Contact">Contact</a>
+    </>
+  );
+};
+
+export default page;
